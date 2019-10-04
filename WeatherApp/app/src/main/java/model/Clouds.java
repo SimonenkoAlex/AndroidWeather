2de8e513c5650754c50714ba0ccf31643d40cade
SimonenkoAlex <alexsim99.99@mail.ru>
2019-10-04 17:27:11 +0600

    creating a json-response model

---
+++ WeatherApp/app/src/main/java/model/Clouds.java
@@ -0,0 +1,13 @@
+package model;
+
+public class Clouds {
+    private int precipitation; // количество атмосферных осадков
+
+    public int getPrecipitation() {
+        return precipitation;
+    }
+
+    public void setPrecipitation(int precipitation) {
+        this.precipitation = precipitation;
+    }
+}
