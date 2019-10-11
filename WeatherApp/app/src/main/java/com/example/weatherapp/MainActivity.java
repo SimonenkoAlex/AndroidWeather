4771e0d5104943570ee4ec766e8c8c47ebd53832
SimonenkoAlex <alexsim99.99@mail.ru>
2019-10-11 17:05:55 +0600

    connection to the api server and receiving the result

--- WeatherApp/app/src/main/java/com/example/weatherapp/MainActivity.java
+++ WeatherApp/app/src/main/java/com/example/weatherapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.weatherapp;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -8,11 +9,35 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.text.DateFormat;
+import java.text.DecimalFormat;
+import java.util.Date;
+
+import data.JSONWeatherParser;
+import data.WeatherHttpClient;
+import model.Weather;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView cityName;
+    private TextView temp;
+    private ImageView iconView;
+    private TextView description;
+    private TextView humidity;
+    private TextView pressure;
+    private TextView wind;
+    private TextView sunrise;
+    private TextView sunset;
+    private TextView updated;
+
+    Weather weather = new Weather();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +45,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        cityName = (TextView) findViewById(R.id.cityText);
+        iconView = (ImageView) findViewById(R.id.thumbnailIcon);
+        temp = (TextView) findViewById(R.id.tempText);
+        description = (TextView) findViewById(R.id.cloudText);
+        humidity = (TextView) findViewById(R.id.humidText);
+        pressure = (TextView) findViewById(R.id.pressureText);
+        wind = (TextView) findViewById(R.id.windText);
+        sunrise = (TextView) findViewById(R.id.riseText);
+        sunset = (TextView) findViewById(R.id.setText);
+        updated = (TextView) findViewById(R.id.updateText);
+
+        renderWeatherData("Moscow,RU");
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -29,6 +67,45 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+    }
+
+    public void renderWeatherData (String city) {
+        WeatherTask weatherTask = new WeatherTask();
+        weatherTask.execute(new String[]{city + "&units=metric&APPID=ae00dc4b6dd00a9863e5e712e68387bf"});
+    }
+
+    private class WeatherTask extends AsyncTask<String, Void, Weather> {
+        @Override
+        protected void onPostExecute(Weather weather) {
+            super.onPostExecute(weather);
+
+            DateFormat df = DateFormat.getTimeInstance();
+            String sunriseDate = df.format(new Date(weather.place.getSunrise()));
+            String sunsetDate = df.format(new Date(weather.place.getSunset()));
+            String updateDate = df.format(new Date(weather.place.getLastupdate()));
+
+            DecimalFormat decimalFormat = new DecimalFormat("#.#");
+            String tempFormat = decimalFormat.format(weather.currentCondition.getTemperature());
+
+            cityName.setText(weather.place.getCity() + "," + weather.place.getCountry());
+            temp.setText("" + tempFormat + " ⁰C");
+            humidity.setText("Humidity: " + weather.currentCondition.getHumidity() + " %");
+            pressure.setText("Pressure: " + weather.currentCondition.getPressure() + " hPa");
+            wind.setText("Wind: " + weather.wind.getSpeed() + " m/s");
+            sunrise.setText("Sunrise: " + sunriseDate);
+            sunset.setText("Sunset: " + sunsetDate);
+            updated.setText("Last Updated: " + updateDate);
+            description.setText("Condition: " + weather.currentCondition.getCondition() + "(" + weather.currentCondition.getDescription() + ")");
+
+        }
+
+        @Override
+        protected Weather doInBackground(String... strings) {
+            String data = ((new WeatherHttpClient()).getWeatherData(strings[0]));
+            weather = JSONWeatherParser.getWeather(data);
+            Log.v("Data: ", weather.place.getCity());
+            return weather;
+        }
     }
 
     @Override
