0ef3c975967aff7e1e852e413af929a549715bae
SimonenkoAlex <40975682+SimonenkoAlex@users.noreply.github.com>
2022-03-02 11:48:21 +0600

    creating a database model class

---
+++ WeatherApp/app/src/main/java/model/City.java
@@ -0,0 +1,36 @@
+package model;
+
+public class City {
+    private long id;
+    private String name;
+    private int avgTemp;
+
+    City(long id, String name, int avgTemp){
+        this.id = id;
+        this.name = name;
+        this.avgTemp = avgTemp;
+    }
+    public long getId() {
+        return id;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAvgTemp() {
+        return avgTemp;
+    }
+
+    public void setAvgTemp(int avgTemp) {
+        this.avgTemp = avgTemp;
+    }
+
+    @Override
+    public String toString() {
+        return this.name + " : " + this.avgTemp;
+    }
+}
